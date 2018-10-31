a972ec09e7d025a4b5c4fa27037c31ed3c4c68df
kilian.q <kilian.q@iv66.net>
2018-10-31 16:00:25 +0800

    blog

--- routes/blog.js
+++ routes/blog.js
@@ -5,5 +5,6 @@ const Category = require('../controller/category')
 const Post = require('../controller/post')
 
 router.get('/posts/page/:page', Post.getPosts);
+router.get('/post/:id', Post.getPost);
 
 export default router
